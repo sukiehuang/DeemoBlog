a429a403336bb4251b3a9461165f84b8e3eaf2ec
墨染 <kpyu1990@qq.com>
2015-01-03 23:42:24 +0800

    路由管理

---
+++ config/router.js
@@ -0,0 +1,15 @@
+/**
+ * Created by kpyu on 2015/1/3.
+ */
+exports.index = function(res,req){
+
+};
+exports.login = function(res,req){
+
+};
+exports.checkLogin = function(res,req){
+
+};
+exports.manage = function(res,req){
+
+}
